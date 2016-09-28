2cb93e4ea5b50da003f5c7c42cb2bd88b6681889
nginnever <ginneversource@gmail.com>
2016-09-28 15:25:39 -0700

    opencl tests

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 const nooocl = require('nooocl')
 const Server = require('./lib/rpc/jsonrpcserver').JsonRpcServer
 const Connection = require('./lib/connection').Connection
+const RpcClient = require('./lib/rpc/jsonrpcclient').JsonRpcClient
 
 const CLHost = nooocl.CLHost
 
 var host = CLHost.createV12()
 host = new CLHost(1.2)
+
+var hostVersion = host.cl.version
+var someOpenCLValue = host.cl.defs.CL_MEM_COPY_HOST_PTR
+var platform = host.getPlatforms()[0]
+//var allPlatforms = host.getPlatforms()
+console.log(platform)
 
 // configure hardcoded node
 var node = {
@@ -15,15 +22,18 @@ var node = {
     jsonrpc: {
       enable: true,
       username: 'voxelot',
-      password: 'kittens',
+      password: 'bushido',
       host: '127.0.0.1',
-      port: 8333 //29001
+      port: 18232//8333//29001
     }
   }
 }
 
-console.log('tesrt')
-var server = new Server(node)
-var connection = new Connection()
+//var server = new Server(node)
+//var connection = new Connection()
 
-server.enable()
+//server.enable()
+
+var client = new RpcClient(node)
+//client.enable()
+//client.call()
