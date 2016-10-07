812fc908157a9b9aaadd2138f716e232c1fe7bbd
omaralvarez <omar.alvarez@udc.es>
2016-10-07 19:51:17 +0200

    Blake2b OpenCL port and list generation using OpenCL

--- test/gpu-list-gen.cl
+++
@@ -1,6 +0,0 @@
-__kernel void add(__global float *inputA, __global float *inputB, __global float *output) {
-    
-    size_t id = get_global_id(0);
-    output[id] = inputA[id] + inputB[id];
-
-}
--- tests/cpu-list-gen.c
+++ tests/cpu-list-gen.c
@@ -1,17 +1,14 @@
-//Includes
+//
+//  cpu-list-gen.c
+//  gpu-miner
+//
+//  Created by Luis Omar Alvarez Mures on 07/10/2016.
+//  Copyright © 2016 Universidade da Coruña. All rights reserved.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
-#include <CL/cl.h>
-#endif
-
-#define DATA_SIZE 10
-// The maximum size of the .cl file we read in and compile
-#define MAX_SOURCE_SIZE 	(0x200000)
 
 #define BLAKE2B_BLOCKBYTES 128
 #define BLAKE2B_OUTBYTES 64
@@ -22,7 +19,9 @@
 # define CRYPTO_ALIGN(x) __attribute__((aligned(x)))
 #endif
 
-inline uint32_t htole32(uint32_t host_32bits)
+//TODO Inline giving problems htole, load, etc.
+
+uint32_t htole32(uint32_t host_32bits)
 {
     return __builtin_bswap32(host_32bits);
 }
@@ -31,11 +30,14 @@ inline uint32_t htole32(uint32_t host_32bits)
 #define ROTR64(x, y)  (((x) >> (y)) ^ ((x) << (64 - (y))))
 #endif
 
-inline uint64_t load64( const void *src )
+uint64_t load64( const void *src )
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
     uint64_t w;
-    memcpy(&w, src, sizeof w);
+    int i;
+    //memcpy(&w, src, sizeof w);
+    for(i = 0; i < sizeof(w); ++i)
+        (&w)[i] = src[i];
     return w;
 #else
     const uint8_t *p = ( const uint8_t * )src;
@@ -51,7 +53,7 @@ inline uint64_t load64( const void *src )
 #endif
 }
 
-inline void store64( void *dst, uint64_t w )
+void store64( void *dst, uint64_t w )
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
     memcpy(dst, &w, sizeof w);
@@ -69,8 +71,8 @@ inline void store64( void *dst, uint64_t w )
 }
 
 /*static inline unsigned long rotr64( __const unsigned long w, __const unsigned c ) {
-    return ( w >> c ) | ( w << ( 64 - c ) );
-}*/
+ return ( w >> c ) | ( w << ( 64 - c ) );
+ }*/
 
 typedef uint32_t eh_index;
 
@@ -112,25 +114,25 @@ static const uint8_t blake2b_sigma[12][16] =
 };
 
 
-inline int blake2b_increment_counter( eh_hash_state *S, const uint64_t inc )
+int blake2b_increment_counter( eh_hash_state *S, const uint64_t inc )
 {
     S->t[0] += inc;
     S->t[1] += ( S->t[0] < inc );
     return 0;
 }
 
-inline int blake2b_is_lastblock( const eh_hash_state *S )
+int blake2b_is_lastblock( const eh_hash_state *S )
 {
     return S->f[0] != 0;
 }
 
-inline int blake2b_set_lastnode( eh_hash_state *S )
+int blake2b_set_lastnode( eh_hash_state *S )
 {
     S->f[1] = -1;
     return 0;
 }
 
-inline int blake2b_set_lastblock( eh_hash_state *S )
+int blake2b_set_lastblock( eh_hash_state *S )
 {
     if( S->last_node ) blake2b_set_lastnode( S );
     
@@ -264,34 +266,16 @@ int blake2b_final( eh_hash_state *S, uint8_t *out, uint8_t outlen )
 }
 
 void generate_hash(const eh_hash_state * base_state, eh_index g, unsigned char * hash, size_t h_len) {
-
+    
     eh_hash_state state = *base_state;
     eh_index lei = htole32(g);
     blake2b_update(&state, (const unsigned char *) &lei, sizeof(eh_index));
     blake2b_final(&state, hash, h_len);
-
+    
 }
 
 int main(void)
 {
-    cl_context context;
-    cl_context_properties properties[3];
-    cl_kernel kernel;
-    cl_command_queue command_queue;
-    cl_program program;
-    cl_int err;
-    cl_uint num_of_platforms=0;
-    cl_platform_id platform_id;
-    cl_device_id device_id;
-    cl_uint num_of_devices=0;
-    cl_mem inputA, inputB, output;
-    
-    size_t global;
-    htole32(2);
-    
-    float inputDataA[DATA_SIZE]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    float inputDataB[DATA_SIZE]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    float results[DATA_SIZE]={0};
     
     unsigned long n = 200;
     unsigned long k = 9;
@@ -310,109 +294,11 @@ int main(void)
     //TODO We need to create this state with Block info, now it is empty
     eh_hash_state base_state;
     printf("Init Size: %i\n", init_size);
-    for (eh_index i = 0; init_size; ++i) {
-        
-        generate_hash(&base_state, i, tmp_hash, hash_output);
-        
-    }
-    
-    
-    
-    int i;
-    
-    // retreive a list of platforms avaible
-    if (clGetPlatformIDs(1, &platform_id, &num_of_platforms)!= CL_SUCCESS)
-    {
-        printf("Unable to get platform_id\n");
-        return 1;
-    }
-    
-    // try to get a supported GPU device
-    if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &num_of_devices) != CL_SUCCESS)
-    {
-        printf("Unable to get device_id\n");
-        return 1;
-    }
-    
-    // context properties list - must be terminated with 0
-    properties[0]= CL_CONTEXT_PLATFORM;
-    properties[1]= (cl_context_properties) platform_id;
-    properties[2]= 0;
-    
-    // create a context with the GPU device
-    context = clCreateContext(properties,1,&device_id,NULL,NULL,&err);
-    
-    // create command queue using the context and device
-    command_queue = clCreateCommandQueue(context, device_id, 0, &err);
-    
-    // Load kernel source file.
-    printf("Initializing...\n");
-    fflush(stdout);
-    FILE *fp;
-    const char fileName[] = "./gpu-list-gen.cl";
-    size_t source_size;
-    char *source_str;
-    fp = fopen(fileName, "r");
-    if (!fp) {
-        fprintf(stderr, "Failed to load kernel.\n");
-        exit(1);
-    }
-    source_str = (char *)malloc(MAX_SOURCE_SIZE);
-    source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-    fclose(fp);
-    
-    // create a program from the kernel source code
-    program = clCreateProgramWithSource(context, 1, (const char **) &source_str, (const size_t *)&source_size, &err);
-    // compile the program
-    if (clBuildProgram(program, 0, NULL, NULL, NULL, NULL) != CL_SUCCESS)
-    {
-        printf("Error building program\n");
-        return 1;
-    }
-    
-    // specify which kernel from the program to execute
-    kernel = clCreateKernel(program, "add", &err);
-    
-    // create buffers for the input and ouput
-    
-    inputA = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * DATA_SIZE, NULL, NULL);
-    inputB = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * DATA_SIZE, NULL, NULL);
-    output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * DATA_SIZE, NULL, NULL);
-    
-    // load data into the input buffer
-    clEnqueueWriteBuffer(command_queue, inputA, CL_TRUE, 0, sizeof(float) * DATA_SIZE, inputDataA, 0, NULL, NULL);
-    clEnqueueWriteBuffer(command_queue, inputB, CL_TRUE, 0, sizeof(float) * DATA_SIZE, inputDataB, 0, NULL, NULL);
-    
-    // set the argument list for the kernel command
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputA);
-    clSetKernelArg(kernel, 1, sizeof(cl_mem), &inputB);
-    clSetKernelArg(kernel, 2, sizeof(cl_mem), &output);
-    
-    global=DATA_SIZE;
-    
-    // enqueue the kernel command for execution
-    clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global, NULL, 0, NULL, NULL);
-    clFinish(command_queue);
-    
-    // copy the results from out of the output buffer
-    clEnqueueReadBuffer(command_queue, output, CL_TRUE, 0, sizeof(float) *DATA_SIZE, results, 0, NULL, NULL);
-    
-    // print the results
-    printf("output: ");
-    
-    for(i=0;i<DATA_SIZE; i++)
-    {
-        printf("%f ",results[i]);
-    }
-    
-    // cleanup - release OpenCL resources
-    clReleaseMemObject(inputA);
-    clReleaseMemObject(inputB);
-    clReleaseMemObject(output);
-    clReleaseProgram(program);
-    clReleaseKernel(kernel);
-    clReleaseCommandQueue(command_queue);
-    clReleaseContext(context);
+    /*for (eh_index i = 0; init_size; ++i) {
+     
+     generate_hash(&base_state, i, tmp_hash, hash_output);
+     
+     }*/
     
     return 0;
     
