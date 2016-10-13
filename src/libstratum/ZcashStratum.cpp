4b59acc665302e7ba8c32088b1e9aee8a96cd268
Omar Alvarez <omar.alvarez@udc.es>
2016-10-13 16:45:53 +0200

    License info added

--- src/libstratum/ZcashStratum.cpp
+++ src/libstratum/ZcashStratum.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2016 Jack Grigg <jack@z.cash>
+// Copyright (c) 2016 Omar Alvarez <omar.alvarez@udc.es>
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
