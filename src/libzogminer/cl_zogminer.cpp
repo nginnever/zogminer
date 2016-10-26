56d0e001322b37cd4ef1f6df568bed0e5a12f64a
Omar Alvarez <omar.alvarez@udc.es>
2016-10-26 17:46:24 +0200

    Update cl_zogminer.cpp

--- src/libzogminer/cl_zogminer.cpp
+++ src/libzogminer/cl_zogminer.cpp
@@ -30,7 +30,7 @@ using namespace std;
 unsigned const cl_zogminer::c_defaultLocalWorkSize = 32;
 unsigned const cl_zogminer::c_defaultGlobalWorkSizeMultiplier = 4096; // * CL_DEFAULT_LOCAL_WORK_SIZE
 unsigned const cl_zogminer::c_defaultMSPerBatch = 0;
-bool cl_zogminer::s_allowCPU = true;
+bool cl_zogminer::s_allowCPU = false;
 unsigned cl_zogminer::s_extraRequiredGPUMem;
 unsigned cl_zogminer::s_msPerBatch = cl_zogminer::c_defaultMSPerBatch;
 unsigned cl_zogminer::s_workgroupSize = cl_zogminer::c_defaultLocalWorkSize;
